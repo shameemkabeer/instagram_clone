469281cc0fcc86df43d563bd2423324abd749268
shameemkabeer <sudosuwhoami867@gmail.com>
2022-09-15 14:01:44 +0530

    general tab added

--- lib/tabs/general.dart
+++ lib/tabs/general.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class general extends StatelessWidget {
   const general({Key? key}) : super(key: key);
