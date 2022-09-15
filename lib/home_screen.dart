6fb892a8162ed009da0b2fc080ed8ab74f6e783c
shameemkabeer <sudosuwhoami867@gmail.com>
2022-09-15 13:19:45 +0530

    screen chats not updated

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,24 +1,24 @@
-import 'package:flutter/material.dart';
-import 'package:instagram/chat_page.dart';
+// import 'package:flutter/material.dart';
+// import 'package:instagram/chat_page.dart';
 
-class homescreen extends StatelessWidget {
-  const homescreen({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => chatscreen()),
-                );
-              },
-              child: Text("chat screen"))
-        ],
-      ),
-    ));
-  }
-}
+// class homescreen extends StatelessWidget {
+//   const homescreen({Key? key}) : super(key: key);
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         body: Center(
+//       child: Column(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         children: [
+//           ElevatedButton(
+//               onPressed: () {
+//                 Navigator.of(context).push(
+//                   MaterialPageRoute(builder: (context) => chatscreen()),
+//                 );
+//               },
+//               child: Text("chat screen"))
+//         ],
+//       ),
+//     ));
+//   }
+// }
